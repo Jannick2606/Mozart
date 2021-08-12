789e155acdaaeb0fd98135ae08523b2ea2b59227
zbcjaso <zbcjaso@ZBC-S-6BN0W>
2021-08-12 09:24:06 +0200

    Add project files.

---
+++ Mozart/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Mozart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object[] files = new object[] { };
+
+        }
+    }
+}
